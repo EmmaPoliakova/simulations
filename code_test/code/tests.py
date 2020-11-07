e0cd421aacacb76f72eab842126d912a8349f44c
Emma Poliakova <2389098p@student.gla.ac.uk>
2020-11-07 17:48:03 +0000

    test

--- code_test/code/tests.py
+++ code_test/code/tests.py
@@ -1,19 +1,16 @@
 from input_code import solution
 
-
 def get_test_cases():
     return {
         "SMALL_INPUT": [1, 2, 3],
         "LARGE_INPUT": [1, 2, 3] * 1000 + [4],
     }
 
-
 def get_expected_outputs():
     return {
         "SMALL_INPUT": 3,
         "LARGE_INPUT": 4,
     }
-
 
 def test_code():
     test_cases = get_test_cases()
@@ -30,9 +27,8 @@ def test_code():
             failed_test_cases.append(label)
 
     print("Passed", passed_test_cases, "out of", test_cases_count, "test cases.")
-
+    
     if len(failed_test_cases) > 0:
         print("Test cases not passed:", ", ".join(failed_test_cases))
-
 
 test_code()
